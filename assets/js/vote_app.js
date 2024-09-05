fac4ba47e200bb1c24711087907631a531815571
Pawan Upadhyay <104750562+u-pawan@users.noreply.github.com>
2024-09-05 10:40:12 +0530

    Update vote_app.js

--- assets/js/vote_app.js
+++ assets/js/vote_app.js
@@ -14,34 +14,28 @@ btn3.addEventListener("click",incrementScore);
 btn4.addEventListener("click",incrementScore);
 
 function endGame() {
-    var s1 = parseInt(document.getElementById("s1").value);
-    var s2 = parseInt(document.getElementById("s2").value);
-    var s3 = parseInt(document.getElementById("s3").value);
-    var s4 = parseInt(document.getElementById("s4").value);
+    var s1 = parseInt(document.getElementById("s1").innerText);
+    var s2 = parseInt(document.getElementById("s2").innerText);
+    var s3 = parseInt(document.getElementById("s3").innerText);
+    var s4 = parseInt(document.getElementById("s4").innerText);
+    
     var maxScore = s1;
-    var winner = "s1";
+    var winner = "BJP";  
 
     if (s2 > maxScore) {
         maxScore = s2;
-        winner = "s2";
+        winner = "Congress";
     }
 
     if (s3 > maxScore) {
         maxScore = s3;
-        winner = "s3";
+        winner = "Jansena";
     }
 
     if (s4 > maxScore) {
         maxScore = s4;
-        winner = "s4";
+        winner = "SP";
     }
 
     alert("The highest score is " + maxScore + " by " + winner);
-}
-
-var parties = {
-    "BJP": 0,
-    "CONGRESS":0,
-    "JANSENA":0,
-    "SP":0
 }
